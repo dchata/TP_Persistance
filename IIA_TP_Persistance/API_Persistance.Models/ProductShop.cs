b009e00b6171d1937d5eb3458f15bc1eb7c91b38
dchata <chatagnierdylan@gmail.com>
2018-03-28 17:01:38 +0200

    changement model product shop et product visit

--- IIA_TP_Persistance/API_Persistance.Models/ProductShop.cs
+++ IIA_TP_Persistance/API_Persistance.Models/ProductShop.cs
@@ -5,10 +5,43 @@ using System.Text;
 
 namespace API_Persistance.Models
 {
-    public class ProductShop : Product
+    public class ProductShop
     {
+        private int Id;
+        private string Name;
+        private float Price;
+        private int Facets;
+        private int Rack;
         private Shop Shop;
 
+        public string name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        public int rack
+        {
+            get { return Rack; }
+            set { Rack = value; }
+        }
+
+        public int facets
+        {
+            get { return Facets; }
+            set { Facets = value; }
+        }
+
+        public float price
+        {
+            get { return Price; }
+            set { Price = value; }
+        }
+
+        public int id
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
         public Shop shop
         {
             get { return Shop; }
--- IIA_TP_Persistance/API_Persistance.Models/ProductVisit.cs
+++ IIA_TP_Persistance/API_Persistance.Models/ProductVisit.cs
@@ -6,15 +6,48 @@ using System.Threading.Tasks;
 
 namespace API_Persistance.Models
 {
-    public class ProductVisit : Product
+    public class ProductVisit
     {
         #region Properties
+        private int Id;
+        private string Name;
+        private float Price;
+        private int Facets;
+        private int Rack;
         private bool Missing;
         private bool Saved;
         private Visit Visit;
         #endregion
 
         #region Access
+        public string name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        public int rack
+        {
+            get { return Rack; }
+            set { Rack = value; }
+        }
+
+        public int facets
+        {
+            get { return Facets; }
+            set { Facets = value; }
+        }
+
+        public float price
+        {
+            get { return Price; }
+            set { Price = value; }
+        }
+
+        public int id
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
         public bool missing
         {
             get { return Missing; }
